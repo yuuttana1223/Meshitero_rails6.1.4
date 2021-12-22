a0be9f3d523756c769b6a2a445c1662cf266f0d1
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-22 18:55:27 +0900

    13章【投稿機能の作成(6) - 投稿を削除できるようにしよう】

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :post_images, only: [:index, :show, :new, :create]
+  resources :post_images, only: [:index, :show, :new, :create, :destroy]
   get "/homes/about", as: "about"
 end
