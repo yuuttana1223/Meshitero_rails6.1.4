74c2cce2d17d3de404ddedbcc9a71fc507661748
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-01-12 22:23:53 +0900

    11章【投稿機能の作成(4) - 投稿一覧を見れるようにしよう】の修正

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -8,12 +8,20 @@ class PostImage < ApplicationRecord
   validates :image, presence: true
 
   def get_image
-    # カリキュラム(省略)
-    if image.attached?
-      image
-    else
-      "no_image.jpg"
+    unless image.attached?
+      #<Pathname:/Users/yuu/Desktop/Meshitero_rails6.1.4/app/assets/images/no_image.jpg>
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      # file_path = Pathname("#{Rails.root}/app/assets/images/no_image.jpg")
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
+    image
+
+    # カリキュラム(省略)
+    # if image.attached?
+    #   image
+    # else
+    #   "no_image.jpg"
+    # end
 
     # 省略なし(self付き)
     # if self.image.attached?
